58e050ed6f5e0764c07e189d6554b3a3f0c986a2
zhanjx-zsy-k <42429782+zhanjx-zsy-k@users.noreply.github.com>
2025-09-05 15:18:51 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'cccd8c24-2aaa-40a6-ad53-64692e0bbdf4';
 let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
@@ -4568,3 +4568,4 @@ async function nginx() {
 	`
     return text;
 }
+
